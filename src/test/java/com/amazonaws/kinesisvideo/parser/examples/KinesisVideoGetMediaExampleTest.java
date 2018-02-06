f365c5933c347185eb3daf7eee7c485b52379a84
sivasoma <Lakshmi9$>
2018-02-06 15:09:56 -0800

    Sample GetMedia Application

--- src/test/java/com/amazonaws/kinesisvideo/parser/examples/KinesisVideoGetMediaExampleTest.java
+++ src/test/java/com/amazonaws/kinesisvideo/parser/examples/KinesisVideoGetMediaExampleTest.java
@@ -4,12 +4,14 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 
 public class KinesisVideoGetMediaExampleTest {
 
+    @Ignore
     @Test
     public void testExample() throws InterruptedException, IOException {
         KinesisVideoGetMediaExample example = KinesisVideoGetMediaExample.builder().region(Regions.US_WEST_2)
