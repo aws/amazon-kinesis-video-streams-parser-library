112d8976490b4f153bb718feb7f303016814d711
sivasoma <Lakshmi9$>
2018-02-06 15:08:23 -0800

    Sample GetMedia Application

---
+++ src/test/java/com/amazonaws/kinesisvideo/parser/examples/KinesisVideoGetMediaExampleTest.java
@@ -0,0 +1,25 @@
+package com.amazonaws.kinesisvideo.parser.examples;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class KinesisVideoGetMediaExampleTest {
+
+    @Test
+    public void testExample() throws InterruptedException, IOException {
+        KinesisVideoGetMediaExample example = KinesisVideoGetMediaExample.builder().region(Regions.US_WEST_2)
+                .streamName("getmedia-sample-stream")
+                .credentialsProvider(new ProfileCredentialsProvider())
+                .build();
+
+        example.execute();
+    }
+
+}
+
+
